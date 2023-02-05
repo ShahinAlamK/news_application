4fe72931b258fefb00cd2b0c52910262c2f6cd9c
ShahinAlamK <kerons89@gmail.com>
2023-02-05 19:18:40 +0600

    flutter new project

--- lib/widgets/card_widget.dart
+++ lib/widgets/card_widget.dart
@@ -53,7 +53,7 @@ class NewsCard extends StatelessWidget {
                     topLeft: Radius.circular(10),
                     topRight: Radius.circular(10),
                   ),
-                  child:Image.network(thumbnailUrl,
+                  child:thumbnailUrl.isEmpty?Image.asset("assets/img/placeholder.jpg"):Image.network(thumbnailUrl,
                     errorBuilder:(context,error,child){
                     return  Image.asset("assets/img/placeholder.jpg");
                     },
